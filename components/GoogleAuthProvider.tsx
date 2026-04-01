85642aa909f1b232a4d0c320a281b53772399e31
chaipeng19940310 <chaipeng19940310@users.noreply.github.com>
2026-04-01 19:47:56 +0800

    Fix: Add fallback for GOOGLE_CLIENT_ID

--- components/GoogleAuthProvider.tsx
+++ components/GoogleAuthProvider.tsx
@@ -3,8 +3,10 @@
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
 export default function GoogleAuthProvider({ children }: { children: React.ReactNode }) {
+  const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || '57700627465-jgt03dnv2d739rovmbe394ckqsigstde.apps.googleusercontent.com'
+  
   return (
-    <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
+    <GoogleOAuthProvider clientId={clientId}>
       {children}
     </GoogleOAuthProvider>
   )
